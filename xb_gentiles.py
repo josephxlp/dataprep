0f32f0de74e77404d79cb65d5ef1980264422e6c
jzphlp <josephluntadilap@gmail.com>
2025-04-28 16:55:15 +0100

    xb_gentiles.py fixed but fbcha

--- xb_gentiles.py
+++ xb_gentiles.py
@@ -1,35 +1,15 @@
 import os 
 import time 
 import multiprocessing
-from pprint import pprint
-from config.uvars import brchieve_dpath12
-from src.ub_tiles import process_tile
+from config.uvars import brchieve_dpath12, vars_name_path_fn,files_byvariable_fn
+from dataprep.wb_tiles import process_tile
+from dataprep.rutils import read_yaml
 
-# f1path = "/media/ljp238/12TBWolf/ARCHIEVE/ARCHIVE_VRT/format_variable_and_files.yaml"
-# f2path = "/media/ljp238/12TBWolf/ARCHIEVE/ARCHIVE_VRT/vars_vrts.yaml"
-
-# f1path = "/media/ljp238/12TBWolf/ARCHIEVE/ARCHIVE_VRTV2/loadfiles_byvariable.yaml"
-# f2path = "/media/ljp238/12TBWolf/ARCHIEVE/ARCHIVE_VRTV2/vars_txts.yaml"
-#=============================utils
-import yaml
-
-def write_yaml(yaml_data, yaml_file_path):
-    with open(yaml_file_path, 'w') as yaml_file:
-        yaml.dump(yaml_data, yaml_file)
-    print('write_yaml')
-    
-def read_yaml(filename):
-    with open(filename, 'r') as yaml_file:
-        yaml_data = yaml.safe_load(yaml_file)
-        print('read_yaml')
-        return yaml_data
-
-#=============================utils
 
 if __name__ == "__main__":
     ti = time.perf_counter()
-    bpaths = read_yaml(f1path) 
-    gpaths = read_yaml(f2path)
+    bpaths = read_yaml(files_byvariable_fn) 
+    gpaths = read_yaml(vars_name_path_fn)
     os.makedirs(brchieve_dpath12,exist_ok=True)
     os.chdir(brchieve_dpath12)
 
@@ -104,8 +84,10 @@ if __name__ == "__main__":
     print(f'run.time: {tf/60} min(s)')
     
 
- 
+
     
+
+# add a logger here 
     
 
 
